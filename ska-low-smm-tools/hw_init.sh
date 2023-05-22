39fa536cf67ef525e62d7f2531ef8e27b4e66395
root@ska-low-smm <admin@sanitaseg.it>
2023-05-22 14:36:57 +0000

    fix custom rott restore and manage SSH server key creation

--- ska-low-smm-tools/hw_init.sh
+++ ska-low-smm-tools/hw_init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-log="/tmp/rc.local.log"
+log="/tmp/hw_init.log"
+
+if [ ! -f "/etc/ssh/ssh_host_ed25519_key.pub" ]; then
+	echo " = SSH server key missing - running reconfigure" 2>&1 | tee -a $log
+	dpkg-reconfigure openssh-server
+fi
 echo " = Running /etc/ska-low-smm-tools/config_ip_from_e2prom.sh" 2>&1 | tee -a $log
 /etc/ska-low-smm-tools/config_ip_from_e2prom.sh 2>&1 | tee -a $log
 
@@ -7,9 +12,9 @@ echo " = Running apt list --installed > /etc/apt/packages.list" 2>&1 | tee -a $l
 apt list --installed | cut -d[ -f1 > /etc/apt/packages.list | tee -a $log
 
 echo " = Running custom_root restore" 2>&1 | tee -a $log
-pushd /etc/ska-low-smm-tools/custom_root 2>&1 | tee -a $log
-sudo cp -rpv . / 2>&1 | tee -a $log
-popd 2>&1 | tee -a $log
+pushd /etc/ska-low-smm-tools/custom_root
+cp -rpv . / 2>&1 | tee -a $log
+popd
 
 echo " = Running subrack_management_board --init" 2>&1 | tee -a $log
 /home/mnguser/SubrackMngAPI/venv/bin/python /home/mnguser/SubrackMngAPI/subrack_mng_api/subrack_management_board.py --init 2>&1 | tee -a $log
